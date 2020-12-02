f3fb16021133969581ab6a9cc8bfd1b31da0213e
Peter McArthur <peter.n.mcarthur@googlemail.com>
2020-12-02 17:43:42 +0000

    Added part 2

--- days/day2.js
+++ days/day2.js
@@ -4,7 +4,17 @@ const compose = require('../compose');
 const stringToComponents = (password) =>
   /(?<min>\d+)\-(?<max>\d+) (?<char>\w): (?<possiblePassword>\w+)/g.exec(password);
 
-const componentsToTest = ({
+const hasCorrectPositions = ({
+  groups: {
+    min,
+    max,
+    char,
+    possiblePassword,
+  },
+}) =>
+  [min, max].filter(x => possiblePassword.charAt(x - 1) === char).length === 1
+
+const hasCorrectNumber = ({
   groups: {
     min,
     max,
@@ -18,18 +28,28 @@ const componentsToTest = ({
 
 (async () => {
 
-  const isPasswordValid = compose(
+  const data = await fetchData(2);
+
+  const part1Validator = compose(
     stringToComponents,
-    componentsToTest,
+    hasCorrectNumber,
+  )
+  const part2Validator = compose(
+    stringToComponents,
+    hasCorrectPositions,
   )
 
-  const correctPasswordCount = compose(
+  const testPasswords = validator => compose(
     x => x.split(/\n/),
     x => x.filter(y => !!y),
-    x => x.filter(isPasswordValid),
+    x => x.filter(validator),
     x => x.length,
   );
 
-  const data = await fetchData(2);
-  console.log('number of correct passwords: ', correctPasswordCount(data));
+
+  const part1 = testPasswords(part1Validator);
+  console.log('(Part 1) number of correct passwords: ', part1(data));
+
+  const part2 = testPasswords(part2Validator);
+  console.log('(Part 2) number of correct passwords: ', part2(data));
 })();
